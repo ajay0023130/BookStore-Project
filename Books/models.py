d7b55004ad7c0a4e8e113e5ad27fd443dcf41447
ajay0023130 <ajaybabu0046@gmail.com>
2023-12-27 02:27:37 +0530

    create table and add tailwind css

--- Books/models.py
+++ Books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=420)
+    subtitle=models.CharField(max_length=420)
+    description = models.TextField()
+    pages= models.IntegerField()
+    publisher= models.CharField(max_length=420)
+    published= models.DateField()
+    author= models.CharField(max_length=420)
+    # ImageUrl = models.CharField(max_length=600)
+
+    def __str__(self):
+        return f'Book :{self.title} author-name :  {self.author}'
--- Books/views.py
+++ Books/views.py
@@ -3,6 +3,47 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello Books App')
+    context = {
+        
+    'Books':
+    [
+    {   'id':1,
+        'name':"Mastering Django: A Beginner's Guide",
+        "ImageUrl":"https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/71AR6gLlkrL._SY385_.jpg"
+        
+    },
+    {   'id':2,
+        'name':"Mastering Django: A Beginner's Guide",
+        "ImageUrl":"https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/71AR6gLlkrL._SY385_.jpg"
+        
+    },
+     {  'id':3,
+        'name':"Mastering Django: A Beginner's Guide",
+        "ImageUrl":"https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/71AR6gLlkrL._SY385_.jpg"
+        
+    },
+     {  'id':4,
+        'name':"Mastering Django: A Beginner's Guide",
+        "ImageUrl":"https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/71AR6gLlkrL._SY385_.jpg"
+        
+    },
+     {   'id':5,
+        'name':"Mastering Django: A Beginner's Guide",
+        "ImageUrl":"https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/71AR6gLlkrL._SY385_.jpg"
+        
+    },
+     {  'id':6,
+        'name':"Mastering Django: A Beginner's Guide",
+        "ImageUrl":"https://m.media-amazon.com/images/W/MEDIAX_792452-T1/images/I/71AR6gLlkrL._SY385_.jpg"
+        
+    },
+
+    ]
+    
+    }
+    return render(request,'Books/index.html',context)
 
 
+def show(request,id):
+
+    return render(request,'Books/BookDetails.html')
